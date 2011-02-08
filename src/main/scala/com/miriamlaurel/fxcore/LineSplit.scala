0539b986313a9c00315ecf26c8010f398a17d3d0
Alexander Temerev <atemerev@air.local>
2011-02-08 19:20:11 +0100

    Temp analysis changes

--- src/main/scala/com/miriamlaurel/fxcore/LineSplit.scala
+++ src/main/scala/com/miriamlaurel/fxcore/LineSplit.scala
@@ -3,8 +3,8 @@ package com.miriamlaurel.fxcore
 import io.Source
 import java.text.SimpleDateFormat
 import scala.collection._
-import java.util.Date
 import java.io.{FileWriter, BufferedWriter, Writer}
+import java.util.{TimeZone, Date}
 
 /**
  * @author Alexander Temerev
@@ -12,6 +12,8 @@ import java.io.{FileWriter, BufferedWriter, Writer}
 object LineSplit {
 
   val format = new SimpleDateFormat("yyyy-MM-dd")
+  format.setTimeZone(TimeZone.getTimeZone("UTC"))
+
   val writers = mutable.Map[String, Writer]()
 
   def writer(ts: Date) = {
@@ -21,7 +23,7 @@ object LineSplit {
 
   def main(args: Array[String]) {
     Source.fromFile(args(0)).getLines.foreach(line => {
-      val ts = new Date(line.split(',')(0).toLong)
+      val ts = new Date(line.substring(0, 13).toLong)
       writer(ts).write(line + "\n")
     })
     writers.foreach(_._2.flush)
