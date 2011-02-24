c14287faa113aaa79ccb1b3d6b8bfefd0d18a0e1
Alexander Temerev <temerev@miriamlaurel.com>
2011-02-24 01:00:51 +0100

    Documentation for portfolio.

--- src/main/scala/com/miriamlaurel/fxcore/portfolio.scala
+++ src/main/scala/com/miriamlaurel/fxcore/portfolio.scala
@@ -7,13 +7,26 @@ import java.io.Serializable
 import java.util.{Date, UUID}
 
 /**
- * @author Alexander Temerev
+ * @author: Alexander Temerev; temerev@miriamlaurel.com
+ */
+
+/*!# Position
+This is the implementation of a position taken in some asset against the other. For example, it can be
+"-100 EUR / 137 USD", which probably means (dependent on context) that 100 euro had been short-sold for 137 US dollars.
+Thus, a position has following fields:
+
+* primary: amount in primary asset, like "-100 EUR" in the example above;
+* secondary: amount in secondary asset, like "137 USD" in the example above;
+* matching: an UUID of a matching position in some portfolio. In some contexts, if a position is applied to portfolio
+  with matching position, these positions will be merged;
+* timestamp: position's creation time;
+* uuid: this position's UUID.
+
  */
 class Position(val primary: Monetary,
                val secondary: Monetary,
                val matching: Option[UUID] = None,
-               override val timestamp:
-               Date = new Date(),
+               override val timestamp: Date = new Date(),
                override val uuid: UUID = UUID.randomUUID)
   extends Entity with TimeEvent {
 
@@ -30,20 +43,56 @@ class Position(val primary: Monetary,
     this (Monetary(amount, instrument.primary), Monetary(-amount * price, instrument.secondary),
       matching, timestamp, uuid)
 
+  /*!
+  If position's primary amount is positive (a long position), it's secondary amount should be negative,
+  and vice versa.
+   */
   require(primary.amount.signum != secondary.amount.signum)
 
+  /*!
+  An instrument can be constructed from position's primary and secondary asset, like "EUR/USD" (a currency pair).
+   */
   lazy val instrument = Instrument(primary.asset, secondary.asset)
+
+  /*!
+  We can easily calculate a price for 1 unit of primary asset -- it is usually referred as position's price.
+   */
   lazy val price: Decimal = (secondary.amount / primary.amount).abs
+
+  /*!
+  Position can be either long (primary amount is positive) or short (negative). Short positions may be handled
+  differently dependent on asset class; in forex, long and short positions are usually quite symmetrical.
+   */
   lazy val side = if (primary.amount > 0) PositionSide.Long else PositionSide.Short
+
+  /*!
+  Position amount is the absolute value of primary asset amount, i.e. it is always positive. If amount sign
+  matters (it usually does), one should use .primary.amount
+   */
   lazy val amount: Decimal = primary.amount.abs
 
+  /*!
+  Position's profit or loss for any price level can be easily calculated, provided it's expressed in position's
+  secondary asset units.
+   */
   def profitLoss(newPrice: Decimal): Money = {
     Money((newPrice - price) * primary.amount, secondary.asset)
   }
 
-  def profitLoss(q: Quote): Option[Money] =
+  /*!
+  For convenience, profit/loss in secondary asset units can also be calculated from a quote for position's
+  instrument. If quote doesn't contain bid or ask price required for this position, None is returned.
+   */
+  def profitLoss(q: Quote): Option[Money] = {
+    require(q.instrument == this.instrument, "Quote and position instrument should match")
     for (price <- if (side == PositionSide.Long) q.bid else q.ask) yield profitLoss(price)
+  }
 
+  /*!
+  Profit/loss can be expressed in any asset, provided we have all quotes necessary for conversion from
+  position's secondary asset to desired asset. A Market instance is required for that; if conversion can't
+  be done for whatever reason, None is returned.
+   */
   def profitLossIn(asset: Asset, market: Market): Option[Money] = {
     for (q <- market.quote(instrument, amount);
          raw <- profitLoss(q);
@@ -51,12 +100,9 @@ class Position(val primary: Monetary,
          m <- market.convert(raw, asset, side, amount)) yield m
   }
 
-  def close(market: Market): Option[Position] = {
-    for (quote <- market.quote(instrument, amount);
-         price <- if (side == PositionSide.Long) quote.bid else quote.ask)
-    yield new Position(instrument, price, -primary.amount)
-  }
-
+  /*!
+  For currency positions, it may be convenient to express profit/loss value in pips.
+   */
   def profitLossPips(price: Decimal): Decimal = instrument match {
     case cp: CurrencyPair => asPips(cp, (price - this.price) * primary.amount.signum)
     case _ => throw new UnsupportedOperationException("Pips operations are defined only on currency positions")
@@ -66,12 +112,16 @@ class Position(val primary: Monetary,
                                                              s = PositionSide.close(side);
                                                              p <- q(s)) yield profitLossPips(p)
 
+  /*!
+  Two positions can be merged, in such a way that:
+  * Two positions with the same side are merged into a larger position;
+  * Two positions with equal amounts and opposite sides collapse; some profit or loss appears from this operation;
+  * Two positions with different amounts and opposite sides partially collapse; some profit or loss appears from this
+    operation; remaining position is returned.
+   */
   def merge(that: Position): (Option[Position], Money) = {
 
     require(this.instrument == that.instrument)
-
-    // This is highly magical code ported from FI Java implementation once written by me on a sheet of
-    // paper and never challenged since then.
 
     // a, b: initial positions;
     // c: position to collapse;
@@ -99,6 +149,10 @@ class Position(val primary: Monetary,
     return (pos, Money(e2, secondary.asset))
   }
 
+  /*!
+  Merging operations can be expressed as a diff value; it can be "applied" to a portfolio, producing the new
+  portfolio with desired changes.
+   */
   def diff(oldPosition: Option[Position]): PortfolioDiff = oldPosition match {
   // If no old position found for this instrument -> add new position
     case None => new PortfolioDiff(AddPosition(this))
@@ -137,54 +191,104 @@ class Position(val primary: Monetary,
     new Deal(closingPart, this.price, this.timestamp, (oldPosition merge this)._2)
   }
 
-
   override def toString = "POSITION " + instrument + " " + primary + " @ " + price
 }
 
+/*!
+A deal is a closed position, with fixed ("realized") profit/loss.
+ */
 class Deal(val position: Position, val closePrice: Decimal, val closeTimestamp: Date, val profitLoss: Money)
 
+
+/*!
+We can have long and short positions...
+ */
 object PositionSide extends Enumeration {
   val Long, Short = Value
 
+  /*!
+  What quote side to use to open a position?
+   */
   def open(side: PositionSide.Value): OfferSide.Value = side match {
     case Long => OfferSide.Ask
     case Short => OfferSide.Bid
   }
 
+  /*!
+  What quote side to use to close a position?
+   */
   def close(side: PositionSide.Value): OfferSide.Value = side match {
     case Long => OfferSide.Bid
     case Short => OfferSide.Ask
   }
 
+  /*!
+  Reverse the position side.
+   */
   def reverse(side: PositionSide.Value): PositionSide.Value = side match {
     case Long => Short
     case Short => Long
   }
 }
 
+/*!#Portfolio
+There can be many different types of portfolios, but all they have common properties summarized by this trait:
+ */
 trait Portfolio {
 
+  /*!
+  A diff value may be applied to this portfolio, giving new (changed) portfolio;
+   */
   def apply(diff: PortfolioDiff): Portfolio
 
+  /*!
+  List of all portfolio positions;
+   */
   def positions: Iterable[Position]
 
+  /*!
+  List of portfolio positions by instrument. In "strict" portfolio implementation, this list always contain
+  zero or one positions.
+   */
   def positions(instrument: Instrument): Iterable[Position]
 
+  /*!
+  "Accept" operation takes a position and "applies" it to portfolio, producing new portfolio and diff value --
+  set of changes.
+   */
   def <<(position: Position): (Portfolio, PortfolioDiff)
 
+  /*!
+  Total amount of all positions in portfolio (expressed in their primary asset). Not that useful beyond
+  forex portfolios, though...
+   */
   def amount(instrument: Instrument): Decimal = this.positions(instrument).map(_.amount).reduceLeft(_ + _)
 
+  /*!
+  Total profit/loss for all positions in portfolio. Since it can be calculated in any asset (currency), a
+  Market instance is required.
+   */
   def profitLoss(asset: Asset, market: Market): Option[Money] = {
-    // I believe this can be done better
     val plValues = this.positions.map(_.profitLossIn(asset, market))
     if (plValues.exists(_.isEmpty)) None
     else
       Some(Money((for (i <- plValues; v <- i) yield v.amount).foldLeft(Decimal(0))(_ + _), asset))
   }
 
+  /*!
+  By default, total profit/loss expressed in market's pivot asset.
+   */
   def profitLoss(market: Market): Option[Money] = profitLoss(market.pivot, market)
 }
 
+/*!##StrictPortfolio
+A "strict" portfolio doesn't store different positions for the same instrument; only aggregate positions are
+stored. This behavior is consistent with position handling by most ECNs.
+
+Even more "stricter" portfolio implementation is possible for forex market, which automatically collapses positions
+by currencies in its instruments' currency pairs; for example, EUR/CHF can be splitted to EUR/USD and USD/CHF
+positions. This "stricter" portfolio is normally used by banks, but we don't need it at the moment.
+ */
 class StrictPortfolio protected(val map: Map[Instrument, Position]) extends Portfolio {
   def this() = this (Map())
 
@@ -219,11 +323,18 @@ class StrictPortfolio protected(val map: Map[Instrument, Position]) extends Port
     case None => List()
   }
 
+  /*!
+  There can be exactly 0 or 1 positions for each instrument...
+   */
   def position(instrument: Instrument): Option[Position] = map.get(instrument)
 
   def size = map.size
 }
 
+/*!##NonStrictPortfolio
+A "non-strict" portfolio allows multiple positions for the same instrument. This behavior is consistent with
+position handling by most market makers.
+ */
 class NonStrictPortfolio protected(val details: Map[Instrument, Map[UUID, Position]]) extends Portfolio {
 
   def this() = this (Map())
@@ -253,6 +364,11 @@ class NonStrictPortfolio protected(val details: Map[Instrument, Map[UUID, Positi
     new NonStrictPortfolio(newDetails)
   }
 
+  /*!
+  We can merge positions with same instrument into one. This method return new non-strict portfolio (with
+  merged positions) and a diff value containing all changes have been made. Merge operation can produce ("realize")
+  profit or loss, which is stored in a diff value as an "adjustment".
+   */
   def mergePositions(uuids: Set[UUID]): (NonStrictPortfolio, PortfolioDiff) = {
     val toMerge = positions.filter(position => uuids.contains(position.uuid))
     if (toMerge.size == 0) (this, new PortfolioDiff())
@@ -294,6 +410,11 @@ class NonStrictPortfolio protected(val details: Map[Instrument, Map[UUID, Positi
   }
 }
 
+/*!#PortfolioAction
+Portfolio action is a "change" that can be applied to portfolio giving new portfolio (or produced by applying a
+position to portfolio). Haskell aficionados will probably recognize a State monad; actual implementation of monadic
+properties might get done in the near future.
+ */
 sealed abstract class PortfolioAction(val appliedPosition: Position)
 
 case class AddPosition(position: Position) extends PortfolioAction(position)
@@ -304,11 +425,16 @@ case class CreateDeal(deal: Deal) extends PortfolioAction(deal.position)
 
 case class MergePositions(instrument: Instrument, merged: Set[Position], result: Option[Position], adjustment: Money) extends PortfolioAction(null)
 
+/*!#PortfolioDiff
+A diff value is just a list of portfolio actions that can be applied sequentially.
+ */
 class PortfolioDiff(acs: PortfolioAction*) {
   val actions = acs.toList
 
   def +(action: PortfolioAction) = new PortfolioDiff((action :: this.actions): _*)
 }
+
+class ConversionException extends Exception
 
 class Account(
                val portfolio: Portfolio,
@@ -334,44 +460,6 @@ class Account(
     yield new Account(newPortfolio, asset, newBalance, Some(convertedDiff), scale)
   }
 
-  /*
-    def merge(uuids: Set[UUID], market: Market): Option[Account] = {
-      val toMerge = portfolio.positions.filter(position => uuids.contains(position.uuid))
-      require(toMerge.map(_.instrument).toSet.size == 1, "Can't merge positions with different instruments")
-      val instrument = toMerge.head.instrument
-      var merged: Option[Position] = None
-      var adjustment: Money = Zilch
-      try {
-        toMerge.foreach(position => {
-          merged match {
-            case None => merged = Some(position)
-            case Some(pos) => {
-              val (p, m) = pos.merge(position)
-              merged = p
-              val converted = market.convert(m, this.asset, PositionSide.close(position.side), position.amount)
-              converted match {
-                case Some(amount) => adjustment = adjustment + amount
-                case None => throw new ConversionException
-              }
-            }
-          }
-        })
-        var newMap = (portfolio.details(merged.instrument) -- uuids)
-        merged match {
-          case Some(position) => newMap = newMap + (merged.uuid -> merged)
-          case None => // do nothing
-        }
-        val newDetails = portfolio.details + (instrument -> newMap)
-        val newPortfolio = new NonStrictPortfolio(newDetails)
-        val diff = new PortfolioDiff(MergePositions(instrument, toMerge, merged, adjustment))
-        Some(new Account(newPortfolio, this.asset, balance + adjustment, diff, this.scale))
-      }
-      catch {
-        case ConversionException => None
-      }
-    }
-  */
-
   private def convertDiff(diff: PortfolioDiff, market: Market): PortfolioDiff = {
     val newActions = diff.actions.map(_ match {
       case CreateDeal(deal) => CreateDeal(convertDeal(deal, market))
@@ -387,5 +475,3 @@ class Account(
     new Deal(deal.position, deal.closePrice, deal.closeTimestamp, converted.get)
   }
 }
-
-class ConversionException extends Exception
