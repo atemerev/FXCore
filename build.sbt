6fd2396f2943d055c2d445a73a6c921bb02993f3
Alexander Temerev <temerev@miriamlaurel.com>
2014-05-08 18:24:49 +0200

    WIP

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ name := "fxcore"
 
 version := "1.0"
 
-scalaVersion := "2.11.0"
+scalaVersion := "2.10.4"
 
 libraryDependencies ++= Seq(
   "joda-time" % "joda-time" % "2.3",
--- src/main/scala/com/miriamlaurel/fxcore/accounting/Delta.scala
+++ src/main/scala/com/miriamlaurel/fxcore/accounting/Delta.scala
@@ -5,7 +5,7 @@ import com.miriamlaurel.fxcore.Money
 import com.miriamlaurel.fxcore.asset.AssetClass
 import com.miriamlaurel.fxcore.market.{QuoteSide, Market}
 
-case class Delta(override val timestamp: DateTime, override val amount: Money) extends Entry {
+case class Delta(override val timestamp: DateTime, override val amount: Money, reference: String, settled: Boolean = false) extends Entry {
   override def convert(to: AssetClass, market: Market): Option[Delta] = for {
     converted <- market.convert(amount, to, QuoteSide.Ask, amount.amount)
   } yield copy()
