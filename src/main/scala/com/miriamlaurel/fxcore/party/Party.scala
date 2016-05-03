2e5a06cf90e79c3cad6fa428635cc9e1bbdd7480
Alexander Temerev <temerev@miriamlaurel.com>
2016-05-04 00:30:25 +0200

    Party comparison fixed

--- src/main/scala/com/miriamlaurel/fxcore/party/Party.scala
+++ src/main/scala/com/miriamlaurel/fxcore/party/Party.scala
@@ -1,3 +1,10 @@
 package com.miriamlaurel.fxcore.party
 
-case class Party(id: String, description: Option[String] = None)
+case class Party(id: String, description: Option[String] = None) {
+  override def hashCode(): Int = id.hashCode
+
+  override def equals(obj: scala.Any): Boolean = obj match {
+    case Party(oid, _) => id.equals(oid)
+    case _ => false
+  }
+}
