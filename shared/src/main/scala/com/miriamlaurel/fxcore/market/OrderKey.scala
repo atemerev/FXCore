6ae3e92263439970b91ce5c1848659afa6239468
Alexander Temerev <sorhed@gmail.com>
2016-12-12 16:11:59 +0100

    ExecId (optional) added to order key

--- shared/src/main/scala/com/miriamlaurel/fxcore/market/OrderKey.scala
+++ shared/src/main/scala/com/miriamlaurel/fxcore/market/OrderKey.scala
@@ -3,6 +3,6 @@ package com.miriamlaurel.fxcore.market
 import com.miriamlaurel.fxcore.instrument.Instrument
 import com.miriamlaurel.fxcore.party.Party
 
-case class OrderKey(party: Party, instrument: Instrument, side: QuoteSide.Value, id: String) {
+case class OrderKey(party: Party, instrument: Instrument, side: QuoteSide.Value, id: String, execId: Option[String] = None) {
   override def toString = "Order %s %s %s (%s)".format(party, instrument, side, id)
 }
