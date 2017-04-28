9701d839d9c8faa3b55c528062026859184e7d0e
Alexander Temerev <atemerev@dora.home>
2017-04-29 01:24:21 +0200

    OrderKey to case class

--- shared/src/main/scala/com/miriamlaurel/fxcore/market/OrderKey.scala
+++ shared/src/main/scala/com/miriamlaurel/fxcore/market/OrderKey.scala
@@ -3,28 +3,16 @@ package com.miriamlaurel.fxcore.market
 import com.miriamlaurel.fxcore.instrument.Instrument
 import com.miriamlaurel.fxcore.party.Party
 
-class OrderKey(val party: Party, val instrument: Instrument, val side: QuoteSide.Value, val id: String, val execId: Option[String] = None) {
+case class OrderKey(party: Party, instrument: Instrument, side: QuoteSide.Value, id: String, execId: Option[String] = None) {
 
-  override def toString = "Order %s %s %s (%s)".format(party, instrument, side, id)
+  override def toString: String = "Order %s %s %s (%s)".format(party, instrument, side, id)
 
   def canEqual(other: Any): Boolean = other.isInstanceOf[OrderKey]
 
   override def equals(other: Any): Boolean = other match {
-    case that: OrderKey =>
-      (that canEqual this) &&
-        party == that.party &&
-        instrument == that.instrument &&
-        side == that.side &&
-        id == that.id
+    case OrderKey(`party`, `instrument`, `side`, `id`, _) => true
     case _ => false
   }
 
-  override def hashCode(): Int = {
-    val state = Seq(party, instrument, side, id)
-    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
-  }
-}
-
-object OrderKey {
-  def apply(party: Party, instrument: Instrument, side: QuoteSide.Value, id: String, execId: Option[String] = None) = new OrderKey(party, instrument, side, id, execId)
+  override def hashCode(): Int = (party, instrument, side, id).##
 }
